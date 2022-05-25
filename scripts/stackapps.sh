efe91ba691d689333217c28d1163aef6a30e2242
Oleg Valter <o.a.philippov@gmail.com>
2022-05-25 12:39:41 +0300

    improved StackApps post generation

--- scripts/stackapps.sh
+++ scripts/stackapps.sh
@@ -1,11 +1,14 @@
 generate-stackapps \
     --about "
-Following Overcharged is a userscript that provides a set of features improving the \"follow\" feature. Current version includes:
+Following Overcharged (inspired by a [MSE feature request](https://meta.stackexchange.com/q/378980/786798)) is a userscript that provides a set of features improving the \"follow\" feature. Current version includes:
 - Auto-following of questions (disabled by default)
 - Auto-following of answers (disabled by default)
 - \"Unfollow all\" feature to unfollow all posts
 
-The userscript uses the [shared configurer](https://stackapps.com/q/9403/78873) for UserScripters projects as a peer dependency. Please install it if you want to be able to change the default settings.
+This is a \"living\" project, so the feature set is likely to be expanded upon in future versions.
+
+The userscript uses the [shared configurer](https://stackapps.com/q/9403/78873) for UserScripters projects as a peer dependency.
+Please install it if you want to be able to change the default settings.
 This is how the configuration UI looks like:
 
 ![screenshot of the Userscript configurer expanded with 3 options for the Following Overcharged userscript](https://i.stack.imgur.com/wM6kh.png)
@@ -20,4 +23,5 @@ This is how the configuration UI looks like:
     --room "https://chat.stackoverflow.com/rooms/214345" \
     --screenshot-alt "animation of the workflow for unfollowing all posts" \
     --screenshot-url "https://i.stack.imgur.com/j3D6c.gif" \
-    --tag "script"
+    --tag "script" \
+    --tag "posts"
