9294e2cf3b0f891c557f5c06a625f23fcc76c5b8
Oleg Valter <o.a.philippov@gmail.com>
2022-05-25 12:22:18 +0300

    added script-specific info to StackApps post generation workflow

--- scripts/stackapps.sh
+++ scripts/stackapps.sh
@@ -1,13 +1,23 @@
 generate-stackapps \
-    --about "Template repository for UserScripters projects." \
-    --chrome "100.0.4896.127" \
-    --excerpt "This is a placeholder repository and should be customized upon forking" \
-    --install "https://github.com/userscripters/template/raw/master/dist/modern/index.user.js" \
-    --minified "https://github.com/userscripters/template/raw/master/dist/modern/index.min.user.js" \
+    --about "
+Following Overcharged is a userscript that provides a set of features improving the \"follow\" feature. Current version includes:
+- Auto-following of questions (disabled by default)
+- Auto-following of answers (disabled by default)
+- \"Unfollow all\" feature to unfollow all posts
+
+The userscript uses the [shared configurer](https://stackapps.com/q/9403/78873) for UserScripters projects as a peer dependency. Please install it if you want to be able to change the default settings.
+This is how the configuration UI looks like:
+
+![screenshot of the Userscript configurer expanded with 3 options for the Following Overcharged userscript](https://i.stack.imgur.com/wM6kh.png)
+" \
+    --chrome "101.0.4951.67" \
+    --excerpt "Following Overcharged contains various improvements to the \"follow\" feature" \
+    --install "https://github.com/userscripters/following-overcharged/raw/master/dist/modern/index.user.js" \
+    --minified "https://github.com/userscripters/following-overcharged/raw/master/dist/modern/index.min.user.js" \
     --language "TypeScript" \
     --org-name "UserScripters" \
     --org-url "https://github.com/userscripters" \
     --room "https://chat.stackoverflow.com/rooms/214345" \
-    --screenshot-alt "a post being highlighted and de-highlighted after a delay" \
-    --screenshot-url "https://i.stack.imgur.com/55TGz.gif" \
+    --screenshot-alt "animation of the workflow for unfollowing all posts" \
+    --screenshot-url "https://i.stack.imgur.com/j3D6c.gif" \
     --tag "script"
